1185f396aafa9986927d6fb0ef37e3015a7f55d8
Paulin <paulin.bonnefoy@outlook.fr>
2023-10-09 16:40:29 +0200

    First commit

---
+++ src/main/java/com/lebontroc/LebontrocApplication.java
@@ -0,0 +1,13 @@
+package com.lebontroc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebontrocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LebontrocApplication.class, args);
+	}
+
+}
