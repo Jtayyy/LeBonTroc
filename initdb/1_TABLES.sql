3231438be9f3106eabf2df3525492f4dfde7e6c1
Paulin <paulin.bonnefoy@outlook.fr>
2023-10-10 14:18:29 +0200

    Create SQL tables instance

---
+++ initdb/1_TABLES.sql
@@ -0,0 +1,31 @@
+create table users
+(
+    id SERIAL PRIMARY KEY,
+    firstname TEXT not null,
+    lastname TEXT not null,
+    pseudo TEXT not null,
+    email TEXT not null,
+    birthdate date not null,
+    adresse TEXT not null,
+    admin bool not null
+);
+
+create table objects
+(
+    id SERIAL PRIMARY KEY,
+    user_id int not null,
+    name TEXT not null,
+    image TEXT not null,
+    description TEXT not null,
+    condition TEXT not null,
+    type TEXT not null
+);
+
+create table posts
+(
+    id SERIAL PRIMARY KEY,
+    object_id int not null,
+    title TEXT not null,
+    publication date not null,
+    adresse TEXT not null
+);
